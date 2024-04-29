156c644a2b28ca360d2b805abb6013d8e7226c3e
Hoseong15 <dlsghtjd3982@gmail.com>
2024-04-29 12:48:45 +0900

    last

--- src/main/resources/static/javascript/cusList.js
+++ src/main/resources/static/javascript/cusList.js
@@ -1,9 +1,9 @@
 function showAlertAndStay() {
-    alert("첫 화면입니다.");
+    alert("첫 페이지입니다.");
     return false;
 }
 
 function showAlertLastPage(){
-    alert("마지막페이지입니다.");
+    alert("마지막 페이지입니다.");
     return false;
 }
